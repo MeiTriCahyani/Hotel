e9a9f007fd21536099701dd4d5d4b8a638bd8218
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-07-05 17:42:08 +0700

    5 JULI 2023

--- Hotel/Form1.cs
+++ Hotel/Form1.cs
@@ -16,5 +16,35 @@ namespace Hotel
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
